d04cb4a2b345b7c0844e7848ccb5f37257848739
Sayooj V R <sayoojvylappully@gmail.com>
2020-03-05 22:48:40 +0530

    build

---
+++ build/precache-manifest.c531511f8752850449aa55a9090459ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ea169b4de4be2e3bd3c4ded1fe48dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f39e982c06bdc2db8e4",
+    "url": "/static/css/main.358bf6ba.chunk.css"
+  },
+  {
+    "revision": "8e354d0ec928a5532118",
+    "url": "/static/js/2.2181c204.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2181c204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f39e982c06bdc2db8e4",
+    "url": "/static/js/main.b25e9054.chunk.js"
+  },
+  {
+    "revision": "67a231a214cd7646b725",
+    "url": "/static/js/runtime-main.653dfd3d.js"
+  }
+]);
